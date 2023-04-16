73649c8be0270bcea6bb68b5ce24a9f2dd1be8fd
Domenico Caraviello <domenicocaraviello1999@gmail.com>
2023-04-16 19:47:36 +0200

    Il mio primo Progetto Laravel

---
+++ Domenico_Caraviello_FirstProject_Laravel/resources/views/index.blade.php
@@ -0,0 +1,152 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!--CDN Font Awsome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!-- CDN Google Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:ital@1&family=Righteous&family=Unbounded:wght@500&display=swap" rel="stylesheet">
+    <!-- CDN Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <title>Play For One</title>
+    <!-- My Css -->
+    <link rel="stylesheet" href="css\style.css">
+</head>
+<body>
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">
+                    <img src="css\Media\Loghi\Cattura-removebg-preview.png" alt="logo_play_for_one">
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="offcanvas offcanvas-start text-bg-dark" tabindex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
+                    <div class="offcanvas-header">
+                        <i class="fa-solid fa-film"></i>
+                        <h5 class="offcanvas-title" id="offcanvasDarkNavbarLabel">Play For One Menu</h5>
+                        <i class="fa-solid fa-film"></i>
+                        <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                        
+                    </div>
+                    <div class="offcanvas-body">
+                        <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+                            <li class="nav-item">
+                                <a class="nav-link active" aria-current="page" href="#">Azione</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Avventura</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Fantasy</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Drammatico</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Horror</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Giallo</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Sci-Fi</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Commedia</a>
+                            </li>
+                            
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    </header>
+    
+    <main>
+        <h1> - SERIE TV DI TENDENZA BY PLAY FOR ONE - </h1>
+        <hr>
+        <div class="container-fluid pt-5 mt-5 ">
+            <div class="row">
+                <div class="col-12 col-md-12">
+                    <div class="card text-bg-dark mb-1">
+                        <div class="row g-0">
+                            <div class="col-md-2 d-flex justify-content-center">
+                                <img src="css\Media\Copertine-Immagini\Peaky Blinders.jpg" class="img-fluid rounded-start" alt="peaky">
+                            </div>
+                            <div class="col-md-10 py-2">
+                                <div class="card-body">
+                                    <h5 class="card-title">Peaky Blinders</h5>
+                                    <a href="https://www.comingsoon.it/serietv/peaky-blinders/1861/scheda/" class="text">
+                                        <p class="card-text d-flex justify-content-start ">Peaky Blinders, la serie creata da Steven Knight, è un crime drama ambientato a Birmingham negli Anni '20 del Novecento, subito dopo la Prima Guerra Mondiale. La storia racconta di una famiglia di gangster, gli Shelby, conosciuta in città come i Peaky Blinders per l'usanza di cucire una lama di rasoio nella visiera dei propri berretti. A tenere le redini 
+                                            ....continua a leggere</a></p>
+                                            <div class="miao">
+                                                <iframe width="560" height="250" src="https://www.youtube-nocookie.com/embed/hGP_ZyrHN68" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <hr>
+                    <div class="row ">
+                        <div class="col-12 col-md-12">
+                            <div class="card text-bg-dark mb-1">
+                                <div class="row g-0">
+                                    <div class="col-md-2 d-flex justify-content-center">
+                                        <img src="css\Media\Copertine-Immagini\The Tudors.jpg" class="img-fluid rounded-start" alt="tudors">
+                                    </div>
+                                    <div class="col-md-10 py-2">
+                                        <div class="card-body">
+                                            <h5 class="card-title">The Tudors</h5>
+                                            <a href="https://www.comingsoon.it/serietv/i-tudors/76/scheda/" class="text">
+                                                <p class="card-text d-flex justify-content-start ">La vita del giovane Enrico VIII, re d'Inghilterra, è tutt'altro che facile. Mentre la moglie non riesce a dargli un erede maschio, Enrico cerca di sfogare fuori dal letto nuziale i suoi desideri sessuali. Il sovrano deve anche guardarsi dagli intrighi e dai tradimenti di corte: chi si definisce legittimo erede al trono tenta di ostacolarlo in ogni modo, e al re spetta valutare come affrontare la sfida del cugino re di Francia.....continua a leggere</a></p>
+                                                <div class="miao">
+                                                    <iframe width="560" height="250" src="https://www.youtube-nocookie.com/embed/WXNf9dxnQrM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <hr>
+                    <div class="container-fluid">
+                        <div class="row ">
+                            <div class="col-12 col-md-12">
+                                <div class="card text-bg-dark mb-1">
+                                    <div class="row g-0">
+                                        <div class="col-md-2 d-flex justify-content-center">
+                                            <img src="css\Media\Copertine-Immagini\Vikings.jpg" class="img-fluid rounded-start" alt="vikings" >
+                                        </div>
+                                        <div class="col-md-10 py-2">
+                                            <div class="card-body">
+                                                <h5 class="card-title">Vikings</h5>
+                                                <a href="https://www.comingsoon.it/serietv/vikings/331/scheda/" class="text">
+                                                    <p class="card-text d-flex justify-content-start ">Ambientata nel IX secolo, la serie tv Vikings racconta le avventure del grande guerriero vichingo Ragnar Lothbrok (Travis Fimmel, C'era una volta Steve McQueen), determinato a esplorare il mondo attraversando le acque dell'oceano. Ciò che guida il guerriero è la sua ambizione e la sua sete di conoscenza, unite alla credenza che gli dèi gli siano benevoli e che gli mandino continuamente segnali dalla natu...continua a leggere</p></a>
+                                                    <div class="miao">
+                                                        <iframe width="560" height="250" src="https://www.youtube-nocookie.com/embed/isDJv84NEkA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    <hr>
+                    </main>
+                    
+                    <footer>
+                        
+                    </footer>
+                    
+                    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+                </body>
+                </html>
